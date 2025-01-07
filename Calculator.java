2b24a02371f93267a1a7243370bb31f9b2707381
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-07 19:59:35 +0530

    Addition code

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,8 @@
 import java.util.*;
 public class Calculator {
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
 
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
@@ -8,5 +11,6 @@ public class Calculator {
         num1 = sc.nextDouble();
         System.out.println("Enter second number: ");
         num2 = sc.nextDouble();
+        System.out.println("Sum: " + add(num1, num2));
     } 
 } 
