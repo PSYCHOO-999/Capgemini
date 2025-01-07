8dd1371e897abd65c3aecab7437f14ba65550896
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-07 19:48:16 +0530

    Boilerplate

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,12 @@
+import java.util.*;
 public class Calculator {
 
     public static void main(String[] args) { 
-        double a = 4; 
-        double b = 5;
+        Scanner sc = new Scanner(System.in);
+        double num1, num2;
+        System.out.println("Enter first number: ");
+        num1 = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        num2 = sc.nextDouble();
     } 
 } 
