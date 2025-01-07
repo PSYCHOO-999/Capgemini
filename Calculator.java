bf88d331ef336243d842f0c3a5e84f3c0859cf33
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-01-07 20:02:14 +0530

    Subtraction Code

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,9 @@ public class Calculator {
     public static double add(double num1, double num2) {
         return num1 + num2;
     }
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
 
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
@@ -12,5 +15,6 @@ public class Calculator {
         System.out.println("Enter second number: ");
         num2 = sc.nextDouble();
         System.out.println("Sum: " + add(num1, num2));
+        System.out.println("Difference: " + subtract(num1, num2));
     } 
 } 
